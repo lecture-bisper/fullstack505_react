c96d27ef51513bf75bf6dc8d0d755282f2137647
lecture-bisper <bisper@outlook.kr>
2023-06-30 10:03:03 +0900

    1. 비동기 방식 처리 응답을 위한 서버 생성.

---
+++ AsyncServer/src/main/java/com/bitc/asyncserver/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.bitc.asyncserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+//    addMapping(url) : 지정한 패턴에 맞는 페이지에 대해서 접근 권한을 확인
+//    allowedOrigins(url) : 접근을 허용할 외부 url
+    registry
+        .addMapping("/**")
+        .allowedOrigins("http://localhost:3000");
+  }
+}
+
+
+
+
+
+
+
+
