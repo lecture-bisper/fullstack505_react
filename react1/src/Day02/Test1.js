506b97e52efb9438650412f16e228cf07575ec2b
lecture-bisper <bisper@outlook.kr>
2023-07-14 16:39:14 +0900

    1. 엘리먼트란?

---
+++ react1/src/Day02/Test1.js
@@ -0,0 +1,38 @@
+// JSX 문법을 사용하여 작성한 코드
+const element = <h1>hello world!!</h1>;
+
+//   실제로 생성된 JS Object 타입
+{
+  type: 'button',
+  props: {
+      className: 'bg-green',
+      children: {
+        type: 'b',
+        props: {
+          children: 'hello world'
+      }
+    }
+  }
+}
+// 위의 object 타입이 구현된 모습
+<button class="bg-green">
+  <b>hello, world</b>
+</button>
+
+
+React.createElement(
+  type,
+  [props],
+  [...children]
+)
+
+
+
+
+
+
+
+
+
+
+
--- react1/src/index.js
+++ react1/src/index.js
@@ -4,17 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import App2 from "./App2";
+import Clock from "./Day02/Clock";
+import Car from "./Day02/Car";
 
 // index.html 파일에서 id값이 'root' 태그를 찾아서 리액트 방식으로 root 태그를 생성
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // render() : 화면을 생성하는 리액트 메소드
-root.render(
-  // JSX 문법을 사용하여 화면을 생성
-  <React.StrictMode>
-    <App />
-    <App2 />
-  </React.StrictMode>
-);
+// root.render(
+//   // JSX 문법을 사용하여 화면을 생성
+//   <React.StrictMode>
+//     <App />
+//     <App2 />
+//   </React.StrictMode>
+// );
+
+// root.render(
+//   <React.StrictMode>
+//     <Clock />
+//   </React.StrictMode>
+// )
+
+// setInterval : 자바스크립트의 타이머 함수, 지정된 시간마다 콜백함수를 실행
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <Clock />
+      {/* 부모 컴포넌트 */}
+      <Car />
+    </React.StrictMode>
+  )
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
