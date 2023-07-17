c802c1c0f51b93695a957c123f9be9d8ea44d5d0
lecture-bisper <bisper@outlook.kr>
2023-07-17 15:54:43 +0900

    1. state의 정의 2. 클래스 컴포넌트와 생명주기 메소드 3. Hooks란 4. useState 5. useEffect 6. useRef

---
+++ react2/src/App4.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import SetStateClass from "./day03/SetStateClass";
+import SetStateFunction from "./day03/SetStateFunction";
+import NotificationList from "./day03/NotificationList";
+import Counter from "./day03/Counter";
+import CounterUseState from "./day03/CounterUseState";
+import CounterUseEffect from "./day03/CounterUseEffect";
+import VolumeControl from "./day03/VolumeControl";
+import TextInputWithFocusButton from "./day03/TextInputWithFocusButton";
+
+function App4() {
+  return (
+    <div>
+      <SetStateClass />
+
+      <hr />
+
+      <SetStateFunction />
+
+      <hr />
+
+      <NotificationList />
+
+      <hr />
+
+      <Counter />
+      <br />
+      <CounterUseState />
+      <br />
+      <CounterUseEffect />
+      <br />
+      <VolumeControl />
+      <br />
+      <TextInputWithFocusButton />
+
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    </div>
+  )
+}
+
+export default App4;
--- react2/src/index.js
+++ react2/src/index.js
@@ -5,13 +5,15 @@ import App from './App';
 import App2 from './App2';
 import reportWebVitals from './reportWebVitals';
 import App3 from "./App3";
+import App4 from "./App4";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/*<App />*/}
     {/*<App2 />*/}
-    <App3 />
+    {/*<App3 />*/}
+    <App4 />
   </React.StrictMode>
 );
 
